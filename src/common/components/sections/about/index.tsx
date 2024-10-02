f7a07f36e293b6511f201db0387553a2e924f848
love1119 <goldenstar007.dev@gmail.com>
2024-10-01 23:44:20 -0400

    update About me section

--- src/common/components/sections/about/index.tsx
+++ src/common/components/sections/about/index.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import { useSectionInView } from "@/common/lib/hooks";
-import portfolioImg from "@/../public/images/photo.jpg";
-import { smoothScrollTo } from "@/common/lib/utils";
-import SectionDivider from "@/common/components/shared/section-divider";
-import SectionHeading from "@/common/components/shared/section-heading";
-import { motion, useScroll, useTransform } from "framer-motion";
-import Image from "next/image";
-import { useRef } from "react";
-import Link from "next/link";
+import { useSectionInView } from '@/common/lib/hooks';
+import portfolioImg from '@/../public/images/photo.jpg';
+import { smoothScrollTo } from '@/common/lib/utils';
+import SectionDivider from '@/common/components/shared/section-divider';
+import SectionHeading from '@/common/components/shared/section-heading';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import Image from 'next/image';
+import { useRef } from 'react';
+import Link from 'next/link';
 
 export default function About() {
-  const { ref } = useSectionInView("about", 0.4);
+  const { ref } = useSectionInView('about', 0.4);
   const divRef = useRef<HTMLDivElement>(null);
 
   const { scrollYProgress } = useScroll({
     target: divRef,
-    offset: ["0 1", "1.33 1"],
+    offset: ['0 1', '1.33 1'],
   });
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
@@ -44,28 +44,26 @@ export default function About() {
             <div className="text-md relative z-40 flex flex-col gap-3 font-semibold tracking-wide text-primary lg:absolute lg:right-0 lg:top-[27%] lg:block lg:max-w-[580px] lg:text-start lg:text-lg xl:top-1/3 xl:h-[442px] xl:max-w-[650px]">
               <div className="flex h-full flex-col justify-center gap-6">
                 <span>
-                  I&apos;m Full-stack developer with expertise in TypeScript,
-                  React, Next.js, Node.js, and database technologies
-                  (MongoDB/PostgreSQL/MySQL) utilizing Prisma for efficient data
-                  access. Proven ability to deliver user-centered web
-                  applications with a focus on intuitive experiences and user
-                  interaction.
+                  {`A results-oriented Full Stack Developer with 12 years of hands-on experience delivering high-performance web applications. My
+                    background includes extensive experience collaborating with cross-functional teams and stakeholders to ensure seamless project
+                    delivery from concept to deployment. I bring a balanced approach to development, focusing on both the technical architecture
+                    and end-user experience.`}
                 </span>
                 <span>
-                  Seeking for Web Development opportunities where I can leverage
-                  my skills to create meaningful connections between products
-                  and users.
+                  {`With a strong foundation in agile practices, I have consistently driven successful project outcomes while adapting to shifting
+                    priorities and client needs. My experience working as a freelancer has further refined my ability to manage projects independently,
+                    meet tight deadlines, and maintain clear communication with clients to ensure project goals are met efficiently and effectively.`}
                 </span>
                 <p className="flex flex-col items-start sm:items-center lg:items-start">
                   <span>So if you are interested,</span>
                   <Link
-                    href={"contact"}
+                    href={'contact'}
                     onClick={(e) => {
-                      smoothScrollTo({ e, id: "contact" });
+                      smoothScrollTo({ e, id: 'contact' });
                     }}
                     className="w-52 lg:w-40"
                   >
-                    <span className="bg-[#ffcbb4] text-2xl font-bold uppercase dark:bg-[#ddbea9] lg:normal-case">
+                    <span className="text-2xl font-bold uppercase lg:normal-case">
                       Contact me!
                     </span>
                   </Link>
@@ -74,7 +72,7 @@ export default function About() {
             </div>
             <div className="absolute z-30 hidden lg:left-0 lg:top-1/4 lg:block">
               <div className="relative h-72 w-72 lg:h-[380px] lg:w-[380px] xl:h-[470px] xl:w-[470px]">
-                <div className="absolute inset-0 z-20 rounded-full bg-gradient-to-b from-[#ffcbb4] via-[#e0afa0] to-[#e29578] transition-opacity group-hover:opacity-60"></div>
+                <div className="absolute inset-0 z-20 rounded-full transition-opacity group-hover:opacity-60"></div>
                 <div className="absolute inset-0">
                   <Image
                     src={portfolioImg}
